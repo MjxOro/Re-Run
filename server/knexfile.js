3f7ffce6a675654492b18a48c17a415f3586b3b2
Matthew Oro <MatthewEOro@gmail.com>
2021-08-11 01:32:18 -0400

    create file structure for project

---
+++ server/knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+			host: "127.0.0.1",
+			user: "root",
+			password: "rootroot",
+			database: "knexcda",
+			charset: "utf8",
+    }
+  },
+
+};
