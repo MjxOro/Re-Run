ebb3b893c8653af6c2afad4df72a7042700bfcdf
Matthew Oro <MatthewEOro@gmail.com>
2021-08-12 03:27:03 -0400

    install framer motion

---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,19 @@
+import react from 'react'
+-
+class LandingPage extends react.Component{
+	state ={
+
+	}
+	componentDidMount = () =>{
+
+	}
+
+	render = () =>{
+		return(
+			<section>
+				
+			</section>
+		)
+	}
+}
+export default LandingPage
