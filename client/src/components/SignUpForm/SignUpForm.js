b79f3f446bb7206794f8af2205a7aa36e8a369ff
Matthew Oro <MatthewEOro@gmail.com>
2021-08-17 02:29:07 -0400

    start signup page

---
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,25 @@
+
+import { Link } from 'react-router-dom'
+import './SignUpForm.scss'
+
+const SignUpForm = () =>{
+
+	return(
+		<section className='signup'>
+			<form className='signup__form'>
+				<h1 className='signup__title'>Sign Up</h1>
+				<label className='signup__label'>Username</label>
+				<input name='username' className='signup__username' placeholder='Username'/>
+				<label className='signup__label'>Email</label>
+				<input name='email' className='signup__username' placeholder='Jeff@apple.com'/>
+				<label className='signup__label'>Password</label>
+				<input name='password' className='signup__username' placeholder='Choose a strong password'/>
+				<button className='signup__button'>Create Account</button>
+				<div className='signup__login-container'>
+					<p className='signup__text'>Already have an account? </p>
+					<Link to='/login' className='signup__link'>Log in</Link>
+				</div>
+			</form>
+		</section>
+	)
+}
---
+++ client/src/pages/SignUp/SignUp.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+
+class SignUp extends React.Component{
+	state = {
+
+	}
+
+	componentDidMount = () =>{
+	}
+
+	render = () =>{
+		return(
+			<>
+			</>
+		)
+	}
+
+}
+export default SignUp
