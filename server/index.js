d2381037da48810d5db4aaf2aa46ac229f045c38
Matthew Oro <MatthewEOro@gmail.com>
2021-08-23 23:50:30 -0400

    add static file folder

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const authorize = require("./middleware/authorize")
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('dev'))
 
 
