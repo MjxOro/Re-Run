0f68b3c6adfe6bafe630165e39fbd80c805e046e
Matthew Oro <MatthewEOro@gmail.com>
2021-08-30 13:06:47 -0400

    prettier formatted

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -1,95 +1,114 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Hero from '../../components/Hero/Hero'
-import MainContent from '../../components/MainContent/MainContent'
-import { Route } from 'react-router-dom'
-import axios from 'axios'
+import React from "react";
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+import MainContent from "../../components/MainContent/MainContent";
+import { Route } from "react-router-dom";
+import axios from "axios";
 
-class MainPage extends React.Component{
-	state = {
-		index: 0,
-		currentUser: null,
-		allPost: null,
-		premiumPosts: [],
-	}
-	componentDidMount = () =>{
-		const token = sessionStorage.getItem("token")
-		axios.get((process.env.REACT_APP_API_URL || "")+'/secure/current/user', {
-			headers: {
-				authorization: `Bearer ${token}`,
-			}
-		})
-		.then(res =>{
-			this.setState({currentUser: res.data})
-			return (axios.get((process.env.REACT_APP_API_URL || "")+'/secure/all/postings',{headers:{authorization: `Bearer ${token}`}}))
-		})
-		.then(res =>{
-			console.log(res.data)
-			const premium = res.data.filter(post => {return post.premium === true})
-			console.log(premium)
-			const premiumIndex = Math.floor(premium.length / 2)
-			this.setState({
-				allPost: res.data,
-				premiumPosts: premium,
-				index: premiumIndex,
-			})
-		})
-		.catch(err =>{
-			console.log(err)
-		})
+class MainPage extends React.Component {
+  state = {
+    index: 0,
+    currentUser: null,
+    allPost: null,
+    premiumPosts: [],
+  };
+  componentDidMount = () => {
+    const token = sessionStorage.getItem("token");
+    axios
+      .get((process.env.REACT_APP_API_URL || "") + "/secure/current/user", {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        this.setState({ currentUser: res.data });
+        return axios.get(
+          (process.env.REACT_APP_API_URL || "") + "/secure/all/postings",
+          { headers: { authorization: `Bearer ${token}` } }
+        );
+      })
+      .then((res) => {
+        console.log(res.data);
+        const premium = res.data.filter((post) => {
+          return post.premium === true;
+        });
+        console.log(premium);
+        const premiumIndex = Math.floor(premium.length / 2);
+        this.setState({
+          allPost: res.data,
+          premiumPosts: premium,
+          index: premiumIndex,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-		const timer = sessionStorage.getItem("timer")
-		if(!timer){
-			sessionStorage.setItem("timer", (+ new Date ()))
-		}
-		else{
-			const now = + new Date()
-			const timeCheck = now - timer
-			if(timeCheck > 300000){
-				axios.put((process.env.REACT_APP_API_URL || "")+'/secure/add/points',{},{headers: {authorization: `Bearer ${token}`}})
-				.then(()=>{
-					sessionStorage.setItem("timer",(+ new Date ()))
-				})
-				.catch(err =>{
-					console.log(err)
-				})
-			}
-		}
-		console.log(sessionStorage.getItem("timer"))
+    const timer = sessionStorage.getItem("timer");
+    if (!timer) {
+      sessionStorage.setItem("timer", +new Date());
+    } else {
+      const now = +new Date();
+      const timeCheck = now - timer;
+      if (timeCheck > 60000) {
+        axios
+          .put(
+            (process.env.REACT_APP_API_URL || "") + "/secure/add/points",
+            {},
+            { headers: { authorization: `Bearer ${token}` } }
+          )
+          .then(() => {
+            sessionStorage.setItem("timer", +new Date());
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }
+    console.log(sessionStorage.getItem("timer"));
+  };
+  componentDidUpdate = (prevProps, prevState) => {};
 
-	}
-	componentDidUpdate = (prevProps, prevState) =>{
-	
+  handleSlideLeft = () => {
+    if (this.state.index > -1) {
+      this.setState({ index: this.state.index - 1 });
+    }
+  };
+  handleSlideRight = () => {
+    if (this.state.index < this.state.premiumPosts.length) {
+      this.setState({ index: this.state.index + 1 });
+    }
+  };
 
-	}
-
-	handleSlideLeft = () =>{
-		if(this.state.index > -1){
-			this.setState({index: this.state.index - 1})
-		}
-
-	}
-	handleSlideRight = () =>{
-		if(this.state.index < this.state.premiumPosts.length){
-			this.setState({index: this.state.index + 1})
-		}
-
-	}
-
-	render = () =>{
-		return(
-			<>
-				{this.state.currentUser && this.state.allPost && this.state.premiumPosts && this.state.index > -1 &&
-					<>
-					<Route  render ={ (routerProps)=>
-						<Header currentUser={this.state.currentUser} {...routerProps} />
-					}/>
-					<Hero data={!this.state.premium ? this.state.allPost : this.state.premium} index={this.state.index} slideRight={this.handleSlideRight} slideLeft={this.handleSlideLeft}/>
-					<MainContent data={this.state.allPost} />
-					</>
-				}
-			</>
-		)
-	}
+  render = () => {
+    return (
+      <>
+        {this.state.currentUser &&
+          this.state.allPost &&
+          this.state.premiumPosts &&
+          this.state.index > -1 && (
+            <>
+              <Route
+                render={(routerProps) => (
+                  <Header
+                    currentUser={this.state.currentUser}
+                    {...routerProps}
+                  />
+                )}
+              />
+              <Hero
+                data={
+                  !this.state.premium ? this.state.allPost : this.state.premium
+                }
+                index={this.state.index}
+                slideRight={this.handleSlideRight}
+                slideLeft={this.handleSlideLeft}
+              />
+              <MainContent data={this.state.allPost} />
+            </>
+          )}
+      </>
+    );
+  };
 }
-export default MainPage
+export default MainPage;
--- client/src/pages/PostPage/PostPage.js
+++ client/src/pages/PostPage/PostPage.js
@@ -1,102 +1,129 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import jsonData from '../../cardData.json'
-import Post from '../../components/Post/Post'
-import SimpleMap from '../../components/Maps/Maps'
-import axios from 'axios'
-import { Route } from 'react-router-dom'
-import Geocode from 'react-geocode'
+import React from "react";
+import Header from "../../components/Header/Header";
+import jsonData from "../../cardData.json";
+import Post from "../../components/Post/Post";
+import SimpleMap from "../../components/Maps/Maps";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import Geocode from "react-geocode";
 
-class PostPage extends React.Component{
-	state = {
-		heroObj: jsonData,
-		index: 0,
-		currentUser: null,
-		filterPost: null,
-		allUsers: null,
-		loaction: null,
+class PostPage extends React.Component {
+  state = {
+    heroObj: jsonData,
+    index: 0,
+    currentUser: null,
+    filterPost: null,
+    allUsers: null,
+    loaction: null,
+  };
+  componentDidMount = () => {
+    window.scrollTo(0, 0);
+    Geocode.setApiKey(process.env.REACT_APP_GOOGLE_KEY);
+    Geocode.setLocationType("APPROXIMATE");
+    const token = sessionStorage.getItem("token");
+    axios
+      .get((process.env.REACT_APP_API_URL || "") + "/secure/current/user", {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ currentUser: res.data });
+        return axios.get(
+          (process.env.REACT_APP_API_URL || "") + "/secure/all/postings",
+          { headers: { authorization: `Bearer ${token}` } }
+        );
+      })
+      .then((res) => {
+        const filtered = res.data.find((post) => {
+          return post._id === this.props.match.params.id;
+        });
+        console.log(filtered);
+        this.setState({ filterPost: filtered });
+        return axios.get(
+          (process.env.REACT_APP_API_URL || "") + "/secure/all/users",
+          { headers: { authorization: `Bearer ${token}` } }
+        );
+      })
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ allUsers: res.data });
+        return Geocode.fromAddress(this.state.filterPost.location);
+      })
+      .then((res) => {
+        console.log(res.results[0].geometry.location);
+        this.setState({ location: res.results[0].geometry.location });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-	}
-	componentDidMount = () =>{
-		window.scrollTo(0, 0);
-		Geocode.setApiKey(process.env.REACT_APP_GOOGLE_KEY)
-		Geocode.setLocationType("APPROXIMATE")
-		const token = sessionStorage.getItem("token")
-		axios.get((process.env.REACT_APP_API_URL || "")+'/secure/current/user', {
-			headers: {
-				authorization: `Bearer ${token}`,
-			}
-		})
-		.then(res =>{
-			console.log(res.data)
-			this.setState({currentUser: res.data})
-			return (axios.get((process.env.REACT_APP_API_URL || "")+'/secure/all/postings',{headers:{authorization: `Bearer ${token}`}}))
-		})
-		.then(res =>{
-			const filtered = res.data.find(post =>{return post._id === this.props.match.params.id})
-			console.log(filtered)
-			this.setState({filterPost: filtered})
-			return (axios.get((process.env.REACT_APP_API_URL || "") + '/secure/all/users',{headers:{authorization: `Bearer ${token}`}}))
-		})
-		.then(res =>{
-			console.log(res.data)
-			this.setState({allUsers: res.data})
-			return (Geocode.fromAddress(this.state.filterPost.location))
-		})
-		.then(res =>{
-			console.log(res.results[0].geometry.location)
-			this.setState({location: res.results[0].geometry.location})
+    const timer = sessionStorage.getItem("timer");
+    if (!timer) {
+      sessionStorage.setItem("timer", +new Date());
+    } else {
+      const now = +new Date();
+      const timeCheck = now - timer;
+      if (timeCheck > 60000) {
+        axios
+          .put(
+            (process.env.REACT_APP_API_URL || "") + "/secure/add/points",
+            {},
+            { headers: { authorization: `Bearer ${token}` } }
+          )
+          .then(() => {
+            sessionStorage.setItem("timer", +new Date());
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }
+    console.log(sessionStorage.getItem("timer"));
+  };
+  getChatCreation = (data) => {
+    const token = sessionStorage.getItem("token");
+    axios
+      .post(
+        (process.env.REACT_APP_API_URL || "") + "/secure/create/chat",
+        data,
+        { headers: { authorization: `Bearer ${token}` } }
+      )
+      .then((res) => {
+        console.log(res);
+        this.props.history.push("/chat");
+      });
+  };
 
-		})
-		.catch(err =>{
-			console.log(err)
-		})
-
-
-		const timer = sessionStorage.getItem("timer")
-		if(!timer){
-			sessionStorage.setItem("timer", (+ new Date ()))
-		}
-		else{
-			const now = + new Date()
-			const timeCheck = now - timer
-			if(timeCheck > 300000){
-				axios.put((process.env.REACT_APP_API_URL || "")+'/secure/add/points',{},{headers: {authorization: `Bearer ${token}`}})
-				.then(()=>{
-					sessionStorage.setItem("timer",(+ new Date ()))
-				})
-				.catch(err =>{
-					console.log(err)
-				})
-			}
-		}
-		console.log(sessionStorage.getItem("timer"))
-	}
-	getChatCreation = (data) =>{
-		const token = sessionStorage.getItem("token")
-		axios.post((process.env.REACT_APP_API_URL || "") + '/secure/create/chat',data,{headers:{authorization: `Bearer ${token}`}})
-		.then(res =>{
-			console.log(res)
-			this.props.history.push("/chat")
-		})
-
-	}
-	
-
-	render = () =>{
-		return(
-			<>
-				{this.state.currentUser && this.state.filterPost && this.state.allUsers && this.state.location &&
-				<>
-				<Route  render ={(routerProps)=>
-					<Header currentUser={this.state.currentUser} {...routerProps} />
-				}/>
-				<Post getChatCreation={this.getChatCreation} allUsers={this.state.allUsers} currentUser={this.state.currentUser} data={this.state.filterPost} index={this.state.index} />
-				<SimpleMap center={this.state.location}  />
-				</>
-				}
-			</>
-		)
-	}
+  render = () => {
+    return (
+      <>
+        {this.state.currentUser &&
+          this.state.filterPost &&
+          this.state.allUsers &&
+          this.state.location && (
+            <>
+              <Route
+                render={(routerProps) => (
+                  <Header
+                    currentUser={this.state.currentUser}
+                    {...routerProps}
+                  />
+                )}
+              />
+              <Post
+                getChatCreation={this.getChatCreation}
+                allUsers={this.state.allUsers}
+                currentUser={this.state.currentUser}
+                data={this.state.filterPost}
+                index={this.state.index}
+              />
+              <SimpleMap center={this.state.location} />
+            </>
+          )}
+      </>
+    );
+  };
 }
-export default PostPage
+export default PostPage;
